fa30d6a8082b014c8ea38ad89fccdc7cf7fd856e
Ruturaj <24387949+RuturajS@users.noreply.github.com>
2020-10-15 09:34:59 +0530

    Create main_map.py

---
+++ main_map.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.plot([1, 2, 3, 9, 1, 0 ])
+plt.plot([0, 7, 1, 5, 7, 4])
+plt.plot([1 , 5])
+plt.ylabel('Ruturaj Tendencies ')
+plt.xlabel('Ruturaj Mind')
+plt.show()
